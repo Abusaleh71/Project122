33936d4067b0553068ed74e14a6893f5c0cecf48
Abusaleh71 <abusalehr343@gmail.com>
2023-03-30 15:31:48 +0600

    Project122

---
+++ Admin/Home1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Index2.css">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>
+</head>
+<body>
+    <div class="container-fluid text-center" id="Hdiv">
+        <h1><b> Admin pannel</b> </h1>
+    </div>
+          <div class="row">
+            <div class="col-md-4">
+               <h4 id="Title">Controls For Admin<h4>
+                <ul type="none" id="navv">
+                  <li><a href="#">Edit profile</a>
+                  <li><a href="#">Add another Admin</a>
+                  <li><a href="#">Add News</a>
+                  <li><a href="#">Add employee</a>
+                  <li><a href="#">Add videos</a>
+                  <li><a href="#">Manage videos</a>
+                  <li><a href="#">Show feedbacks</a>
+                  <li><a href="#">Total Subscriber</a>
+
+            </div>
+            <div class="col-md-8" id="seconD">
+              <div class="col-sm-6" id="ND">
+                <h1 id="wlcm"><b> Welcome To My Portfolio Website </b><h1>
+              </div>
+            
+            </div>
+          </div>
+         <div class="container-fluide" id="footerdv">
+          <p>@Copyrrigt.Mywebsite.2023</p>
+         </div>             
+
+</body>
+</html>
